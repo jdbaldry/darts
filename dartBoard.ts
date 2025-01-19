5b3781be56cd71c6c4a2a58913bacd1b20cab972
Jack Baldry <jack.baldry@grafana.com>
2025-01-19 12:27:47 +0000

    Hacked together
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- dartBoard.ts
+++ dartBoard.ts
@@ -1,8 +1,40 @@
+enum Game {
+  301 = "301",
+  501 = "501",
+  Cricket = "Cricket",
+}
+
 type Point = {
   x: number;
   y: number;
 };
 
+type Record = {
+  timestamp: number;
+
+  canvas: { width: number; height: number };
+  point: Point;
+
+  game: Game;
+  player: string;
+  thro: Throw;
+};
+
+enum ThrowKind {
+  Double = "D",
+  Single = "S",
+  Triple = "T",
+
+  Floor = "F",
+  Out = "O",
+}
+
+type Throw = {
+  kind: ThrowKind;
+
+  multiplier: number;
+  segment: number;
+};
 /*
 https://www.reddragondarts.com/pages/darts-rules
 
@@ -38,34 +70,195 @@ const OUTER_BULL_COLOR = "green";
 const SEGMENT_A_COLOR = "#2e343a";
 const SEGMENT_B_COLOR = "#fde1d0";
 
-class DartBoard {
-  private ctx: CanvasRenderingContext2D;
+// parseThrow parses throw notation.
+// [t|T] = triple
+// [d|D] = double
+// [o|O] = out
+// [f|F] = floor
+// \d+ = segment
+function parseThrow(score: string): Throw | null {
+  const re = new RegExp("^(?<kind>f)|(?<kind>[dot])?(?<segment>\\d+)$", "i");
+
+  const results = re.exec(score);
+  if (results === null) return null;
+
+  if (results.groups.segment !== undefined) {
+    const segment = parseInt(results.groups.segment);
+    const kind =
+      results.groups.kind !== undefined
+        ? results.groups.kind.toUpperCase()
+        : "S";
+    const multiplier =
+      kind === "T" ? 3 : kind === "D" ? 2 : kind === "O" ? 0 : 1;
+
+    return { kind, segment, multiplier };
+  }
+
+  return { kind: "F", segment: -1, multipler: 0 };
+}
+
+class Dartboard {
   private center: Point;
+  private ctx: CanvasRenderingContext2D;
+  private table: HTMLTableElement;
+  private thro: number = 0;
+  private turn: number = 0;
+  private remaining: number = 301;
+  private out: bool = false;
 
   debug: boolean = false;
 
-  constructor(canvas: HTMLCanvasElement, size: number) {
+  constructor(
+    canvas: HTMLCanvasElement,
+    size: number,
+    score: HTMLTableElement,
+    reset: HTMLButtonElement,
+    xport: HTMLButtonElement,
+    clear: HTMLButtonElement,
+  ) {
     this.ctx = canvas.getContext("2d");
 
     this.ctx.canvas.width = size;
     this.ctx.canvas.height = size;
     this.center = { x: canvas.width / 2, y: canvas.height / 2 };
+    this.table = score;
+
+    clear.addEventListener("click", (event) => {
+      if (window.confirm("Do you really want to clear local storage?")) {
+        localStorage.setItem("records", "");
+      }
+    });
+
+    xport.addEventListener("click", (event) => {
+      const text = localStorage.getItem("records") || [];
+      const name = `${Date.now()}.json`;
+
+      const a = document.createElement("a");
+      const file = new Blob([text], { type: "application/json" });
+      a.href = URL.createObjectURL(file);
+      a.download = name;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    });
+
+    reset.addEventListener("click", (event) => {
+      this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+      this.table.innerHTML = "";
+      this.thro = 0;
+      this.turn = 0;
+      this.remaining = 301;
+      this.out = false;
+
+      board.draw();
+    });
 
     addEventListener("click", (event) => {
+      if (this.out) {
+        return;
+      }
+
       const point: Point = this.getMousePos(this.ctx, event);
 
-      console.log(this.score(point), point);
+      if (point === undefined) {
+        return;
+      }
+
+      const score = this.score(point);
+      const thro = parseThrow(score);
+
+      const record: Record = {
+        timestamp: Date.now(),
+
+        canvas: {
+          width: this.ctx.canvas.width,
+          height: this.ctx.canvas.height,
+        },
+        point,
+
+        game: Game["301"],
+        player: "jdb",
+        thro,
+      };
+
+      const records = localStorage.getJSON("records") || [];
+      records.push(record);
+      localStorage.setJSON("records", records);
+
+      if (this.thro === 0) {
+        this.table.insertRow(this.turn);
+        this.table.rows[this.turn].insertCell(-1).textContent = this.turn + 1;
+        this.table.rows[this.turn].insertCell(-1).textContent = this.remaining;
+      }
+
+      if (thro.kind === "F") {
+        this.table.rows[this.turn].insertCell(-1).textContent = "FLOOR";
+        this.thro++;
+
+        if (this.thro === 3) {
+          this.thro = 0;
+          this.turn++;
+        }
+
+        return;
+      }
+
+      const remaining = this.remaining - thro.segment * thro.multiplier;
+
+      if (remaining === 0 && thro.kind === "D") {
+        this.table.rows[this.turn].insertCell(-1).textContent =
+          `${score} (OUT)`;
+        this.out = true;
+
+        return;
+      }
+
+      if (remaining < 2) {
+        this.table.rows[this.turn].insertCell(-1).textContent =
+          `${score} (BUST)`;
+
+        this.thro = 0;
+        this.turn++;
+
+        return;
+      }
+
+      this.remaining = remaining;
+
+      this.table.rows[this.turn].insertCell(-1).textContent = score;
+      this.table.rows[this.turn].cells[1].textContent = this.remaining;
+      this.thro++;
+
+      if (this.thro === 3) {
+        this.thro = 0;
+        this.turn++;
+      }
+
       this.drawDart(point);
     });
   }
 
-  private getMousePos(ctx: CanvasRenderingContext2D, evt: MouseEvent): Point {
+  // getMousePos returns the mouse position relative to the canvas.
+  // If the mouse is outside the canvas, it returns undefined.
+  private getMousePos(
+    ctx: CanvasRenderingContext2D,
+    evt: MouseEvent,
+  ): Point | undefined {
+    if (
+      evt.clientX < ctx.canvas.offsetLeft ||
+      evt.clientX > ctx.canvas.offsetLeft + ctx.canvas.width ||
+      evt.clientY < ctx.canvas.offsetTop ||
+      evt.clientY > ctx.canvas.offsetTop + ctx.canvas.height
+    ) {
+      return undefined;
+    }
     return {
       x: evt.clientX - ctx.canvas.offsetLeft,
       y: evt.clientY - ctx.canvas.offsetTop,
     };
   }
 
+  // drawDart draws a dart marker.
   private drawDart(point: Point) {
     this.ctx.beginPath();
     this.ctx.moveTo(point.x, point.y);
@@ -187,10 +380,31 @@ class DartBoard {
       return "b25";
     }
 
-    const angle = Math.atan2(point.y - this.center.y, point.x - this.center.x);
-    const segment = Math.floor(((angle + Math.PI) / (Math.PI * 2)) * SEGMENTS);
+    if (distance > OVERALL_DART_BOARD_DIAMETER / 2) {
+      return "f";
+    }
 
-    const segmentScore = SEGMENT_MAPPINGS[(segment + 5) % SEGMENTS].toString();
+    const angle = Math.atan2(point.y - this.center.y, point.x - this.center.x);
+
+    // Calculate the segment hit.
+    let segment = Math.floor(
+      ((angle + Math.PI / SEGMENTS) / (Math.PI * 2)) * SEGMENTS,
+    );
+
+    // Draw the segment hit for debugging.
+    if (this.debug) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(point.x, point.y);
+      this.ctx.lineTo(
+        this.center.x + (Math.cos(angle) * OVERALL_DART_BOARD_DIAMETER) / 2,
+        this.center.y + (Math.sin(angle) * OVERALL_DART_BOARD_DIAMETER) / 2,
+      );
+      this.ctx.strokeStyle = "orange";
+      this.ctx.stroke();
+    }
+
+    const segmentScore = SEGMENT_MAPPINGS[(segment + SEGMENTS + 5) % SEGMENTS];
+
     const multiplier =
       distance <= TREBLE_WIRE_TO_CENTER_BULL &&
       distance > TREBLE_WIRE_TO_CENTER_BULL - DOUBLE_TREBLE_WIDTH
@@ -198,7 +412,9 @@ class DartBoard {
         : distance <= DOUBLE_WIRE_TO_CENTER_BULL &&
             distance > DOUBLE_WIRE_TO_CENTER_BULL - DOUBLE_TREBLE_WIDTH
           ? "d"
-          : "";
+          : distance >= DOUBLE_WIRE_TO_CENTER_BULL
+            ? "o"
+            : "";
 
     return `${multiplier}${segmentScore}`;
   }
@@ -220,6 +436,27 @@ class DartBoard {
   }
 }
 
-const canvas = document.getElementById("dartBoard") as HTMLCanvasElement;
-const board = new DartBoard(canvas, window.innerHeight);
+Storage.prototype.setJSON = function (key, element) {
+  return this.setItem(key, JSON.stringify(element));
+};
+Storage.prototype.getJSON = function (key) {
+  const item = this.getItem(key);
+  if (!item) return undefined;
+
+  return JSON.parse(item);
+};
+
+const table = document.getElementById("score") as HTMLTableElement;
+const reset = document.getElementById("reset") as HTMLButtonElement;
+const canvas = document.getElementById("dartboard") as HTMLCanvasElement;
+const xport = document.getElementById("export") as HTMLButtonElement;
+const clear = document.getElementById("clear") as HTMLButtonElement;
+const board = new Dartboard(
+  canvas,
+  window.innerHeight,
+  table,
+  reset,
+  xport,
+  clear,
+);
 board.draw();
